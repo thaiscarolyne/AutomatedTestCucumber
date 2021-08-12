7075ea71ff6ca42ee09fe83d85a7b027941d0023
Thais Almeida <thaiscarolyne97@gmail.com>
2021-08-11 22:56:44 -0300

    [FIX] Adding gradle task for cucumber

--- AutomatedTestCucumberTEST/src/test/java/Runner.java
+++ AutomatedTestCucumberTEST/src/test/java/Runner.java
@@ -1,11 +1,3 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"},
-        glue = {"scenarios"},
-        features = {"classpath:github.feature"})
 public class Runner {
     public static void main(String[] args){
         io.cucumber.core.cli.Main.main(args);
